205e764396a53fbb50d7da83092746a1c549389e
Jasmine <jsel4635@gmail.com>
2024-09-12 16:33:04 -0700

    Fixed task url.

--- task/urls.py
+++ task/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 app_name = "task"
-urlpatters = [
+urlpatterns = [
     path("", views.index, name="index"),
-    path("add", views.add, name="add"),
+    path("add", views.add, name="add")
 ]
